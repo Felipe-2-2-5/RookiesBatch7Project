cb76ab8d18474f6daf36055895796615fb464c6e
Felipe <phongptk12fpt@gmail.com>
2024-06-11 09:37:51 +0700

    Add new item Add new github action

--- MySelfStudyApp/Controllers/WeatherForecastController.cs
+++ MySelfStudyApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MySelfStudyApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Foggy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
